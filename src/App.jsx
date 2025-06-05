0e095536fad546b0875e33bd87b4af77045234d7
karl <146457676+KarlAzuz@users.noreply.github.com>
2025-06-05 15:39:10 +0000

    word to my mother

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-background">
+      <div className="placeholder-center">
+        placeholder
+      </div>
+    </div>
+  );
+}
+
+export default App;
